7d5a2eca53ce22f318db150d2793afe78a737623
Edmar Almeida <eng.edmar@gmail.com>
2022-09-18 20:38:05 -0300

    Tratamento de erros e exceções

---
+++ erros_eafp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+# EAFP - Easy to Ask Forgiveness than permission
+# Atteintion - You need anticipate all possible erros and
+# treat them individually. That is goog practice.
+try:
+    names = open("names.txt").readlines()
+except FileNotFoundError:
+    print("[Error]  File names.txt noot found!") # Erro se o 
+    # arquivo não for encontrado
+    sys.exit(1)
+    # TODO: Usar um retry
+else:
+    print("Sucesso!") # É ambíguo o uso de else nesse caso,
+    # mas pode ser usado. 
+
+try:
+    print(names[4])
+except IndexError as a:
+    print(f"{str(a)}")
+    sys.exit(1)
+
